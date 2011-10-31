069943d50c434f17d7cbad6676c668b914e9536b
Chris Carter <chris@carterventures.com>
2011-10-31 10:21:19 -0700

    Project created

---
+++ README.md
@@ -0,0 +1,2 @@
+# Knighthouse
+This is a warehouse management system built for the Knights of Columbus.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knighthouse::Application.config.secret_token = '4a2a390a89eade03e5f9b1d64820adc416bb350f3af35903e9e33d5a170ca9461cde85f57e49d447c85ac2d56c3cf0211d51204c19e0feeb77ffa7f2d4277b23'
